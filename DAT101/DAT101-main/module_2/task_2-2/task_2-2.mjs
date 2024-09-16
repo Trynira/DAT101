64052e1bcc0ebe3797ab2410ad00787df2b208be
Mathias Brovig <mathias.brovig@hotmail.no>
2024-09-16 14:11:01 +0200

    pain^2

--- DAT101/DAT101-main/module_2/task_2-2/task_2-2.mjs
+++ DAT101/DAT101-main/module_2/task_2-2/task_2-2.mjs
@@ -63,11 +63,33 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Convert 54 dollars to Norwegian kroner, and print the price for both:
+NOK → USD and USD → NOK.
+Use 76 NOK = 8.6 USD as the exchange rate.
+The answer must be in whole "Kroner" and whole "dollars".*/
+const nok =76/8.6;
+let usd=54;
+let kr=nok*usd;
+usd=usd.toString();
+kr=Math.trunc(kr);
+kr=kr.toString();
+printOut("nok "+kr +", usd "+usd+".");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+/*Create a variable that contains the following text:
+"There is much between heaven and earth that we do not understand."
+● Print the number of characters in the text.
+● Print the character at position number 19.
+● Print the characters starting at position number 35 and 8 characters forward.
+● Print the index at which "earth" starts in the text*/
+let text6="There is much between heaven and earth that we do not understand.";
+printOut(text6)
+let length6 =text6.length;
+length6=length6.toString
+printOut(length6);
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
