09eef38d0614695b12dc44aedefbfe3ff7650083
Mathias Brovig <mathias.brovig@hotmail.no>
2024-10-09 15:45:54 +0200

    Partial completing of task_3-1

--- DAT101/DAT101-main/module_3/task_3-1/task_3-1.mjs
+++ DAT101/DAT101-main/module_3/task_3-1/task_3-1.mjs
@@ -5,39 +5,94 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+let wakeUpTime = 8;
+if (wakeUpTime ==7) {
   printOut("I can take the bus to school.");
+
+} else if (wakeUpTime == 8){
+  printOut("I can take the train to school.");
+} else {
+  printOut("I have to take the car to school.");
 }
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const integer=-10.3;
+if (integer<0){
+  printOut("Negative.");
+} else if (integer ===0) {
+  printOut("Zero.");
+} else {
+  printOut("Positive.")
+}
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const photoSize =Math.floor(Math.random()*8)+1;
+if (photoSize>=4) {
+  printOut("Thank you");
+} else {
+  printOut("The image is too small");
+}
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+//const photoSize =Math.floor(Math.random()*8)+1;
+if (photoSize<4) {
+  printOut("The image is too small");
+} else if (photoSize >=6) {
+  printOut("Image is too large");
+} else {
+  printOut("Thank you");
+}
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList =["January", "February", "March", "April", "May",
+  "June", "July", "August", "September", "October", "November", "December"];
+  const noOfMonth = monthList.length;
+  const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You do not need to take vitamin D");
+}
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+//const monthList =["January", "February", "March", "April", "May",
+//  "June", "July", "August", "September", "October", "November", "December"];
+//  const noOfMonth = monthList.length;
+//  const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You do not need to take vitamin D");
+}
+let days=0;
+const monthList30 = ["April","June","September","November"]
+if (monthList30.includes(monthName)) {
+  printOut(monthName + " has 30 days.");
+} else if (monthName == "February") {
+  printOut(monthName + " has 28 days.");
+} else{
+  printOut(monthName + " has 31 days.");
+} 
 printOut(newLine);
 
-/* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const monthListClosed = ["March","May"]
+if (monthListClosed.includes(monthName)) {
+  printOut("The Art gallery is closed in " + monthName+" due to renovations.");
+} else if (monthName=="April") {
+  printOut("The art gallery has temporary premises in the building next door in April due to renovations, welcome");
+} 
+else {
+  printOut("The art gallery is open in "+ monthName+", Welcome")
+}
